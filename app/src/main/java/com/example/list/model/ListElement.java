532225b597f7b307b7e4c1fee03a38002b9aa8c4
irvingbaez <irvingbaezc@gmail.com>
2020-03-10 22:33:06 -0600

    ListElement updated and ListElementTest created.

--- app/src/main/java/com/example/list/model/ListElement.java
+++ app/src/main/java/com/example/list/model/ListElement.java
@@ -4,16 +4,19 @@ import androidx.annotation.NonNull;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Locale;
 
 public class ListElement implements Comparable{
     private int id;
-    private int parentID;
+    private int parentId;
     private String content;
     private boolean finished;
     private int customIndex;
     private Date date;
+    private ArrayList<ListElement> elements = new ArrayList<>();
 
     private static int currentId;
     private static compareMode mode;
@@ -24,18 +27,18 @@ public class ListElement implements Comparable{
      * Method meant to be used when initializing elements from database.
      *
      * @param id id as found in database.
-     * @param parentID id of parent in the database. Cannot be the same as id.
+     * @param parentId id of parent in the database. Cannot be the same as id.
      * @param content content of the element to be displayed.
      * @param finished current state of the element. Either finished or not.
      * @param customIndex index used to replicate order defined by user.
      * @param date date the element was created.
      */
-    public ListElement(int id, int parentID, String content, int finished, int customIndex, String date) {
-        if(id == parentID)
-            throw new AssertionError("id cannot be the same as parentID");
+    public ListElement(int id, int parentId, String content, int finished, int customIndex, String date) {
+        if(id == parentId)
+            throw new AssertionError("id (" + id + ") cannot be the same as parentID.");
 
         this.id = id;
-        this.parentID = parentID;
+        this.parentId = parentId;
         this.content = content;
         this.customIndex = customIndex;
         this.finished = (finished == 1);
@@ -51,13 +54,12 @@ public class ListElement implements Comparable{
     /**
      * Method meant to be used to create elements on execution time.
      *
-     * @param parentID id of parent in the database. Cannot be the same as id.
+     * @param parentId id of parent in the database. Cannot be the same as id.
      * @param content content of the element to be displayed.
-     * @param customIndex index used to replicate order defined by user.
      */
-    public ListElement(int parentID, String content, int customIndex) {
+    public ListElement(int parentId, String content, int customIndex) {
         this.id = ++ListElement.currentId;
-        this.parentID = parentID;
+        this.parentId = parentId;
         this.content = content;
         this.customIndex = customIndex;
         this.finished = false;
@@ -82,12 +84,32 @@ public class ListElement implements Comparable{
         return 0;
     }
 
+    public void addElement(ListElement element){
+        this.elements.add(element);
+    }
+
+    public void removeElement(ListElement element){
+        this.elements.remove(element);
+    }
+
+    public void insertElement(ListElement element, int newIndex){
+        if(newIndex > this.elements.size()-1)
+            throw new AssertionError("Index is too large for current list.");
+
+        int currentIndex = elements.indexOf(element);
+        Collections.rotate(this.elements.subList(newIndex, currentIndex), -1);
+
+        for(int i = newIndex; i <= currentIndex; i++){
+            this.elements.get(i).customIndex = i;
+        }
+    }
+
     public int getId() {
         return id;
     }
 
-    public int getParentID() {
-        return parentID;
+    public int getParentId() {
+        return parentId;
     }
 
     public String getContent() {
---
+++ app/src/test/java/com/example/list/model/ListElementTest.groovy
@@ -0,0 +1,36 @@
+package com.example.list.model
+
+class ListElementTest extends GroovyTestCase {
+    void testCompareTo() {
+    }
+
+    void testAddElement() {
+    }
+
+    void testRemoveElement() {
+    }
+
+    void testInsertElement() {
+    }
+
+    void testGetId() {
+    }
+
+    void testGetParentId() {
+    }
+
+    void testGetContent() {
+    }
+
+    void testIsFinished() {
+    }
+
+    void testGetCustomIndex() {
+    }
+
+    void testGetDate() {
+    }
+
+    void testSetMode() {
+    }
+}
